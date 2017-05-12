f0d46780bc8b81602c87a45d9005cd3d862663cb
Shammamah Hossain <shammamah.hossain@gmail.com>
2017-05-12 15:30:28 -0400

    Copies root macro from my usual working directory to the required directory.

---
+++ makemacro.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+cp -f  ~/Dropbox/ATLAS/root/macros/$1.cc ~/Documents/root/macros/$1.cc
+cp -f  ~/Dropbox/ATLAS/root/macros/$1.cc /usr/local/Cellar/root6/6.08.06/share/root/macros/$1.cc
+
